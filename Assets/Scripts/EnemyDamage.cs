9ee1df115ba59053fb56b90e6b2e283df9c3522b
Douglas Gammons <dgammo300@caledonian.ac.uk>
2025-03-19 22:45:29 +0000

    Health Bar & Updated Health System
    Health Bar & Updated Health System

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -1,28 +1,33 @@
 using Assets.Enemy.Scripts;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
     GlobalController _controller;
+  
+
 
     private void Start()
     {
         _controller = GlobalController.instance;
+
+       
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
-            _controller.DecreaseHealth(10);
+            _controller.DecreaseHealth(_controller.enemyDamage);
         }
         if (other.CompareTag("Enemyfast"))
         {
-            _controller.DecreaseHealth(5);
+            _controller.DecreaseHealth(_controller.enemyFastDamage);
 
         }
         if (other.CompareTag("Enemyslow"))
         {
-            _controller.DecreaseHealth(20);
+            _controller.DecreaseHealth(_controller.enemySlowDamage);
         }
     }
 }
